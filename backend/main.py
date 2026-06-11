3af068a598f8b28a3802922f5c5a6628f9185f85
VyshnavReddyPinreddy <vyshnavreddy8522@gmail.com>
2026-06-11 22:18:45 +0530

    Added the favourites router

--- backend/main.py
+++ backend/main.py
@@ -3,6 +3,7 @@ from routers.login import router as login_router
 from routers.password_reset import router as password_reset_router
 from routers.chat import router as chat_router
 from routers.voice_query import router as voice_query_router
+from routers.favorites import router as favorites_router
 from fastapi.middleware.cors import CORSMiddleware
 
 app = FastAPI(title="ASHA")
@@ -19,4 +20,5 @@ app.include_router(login_router)
 app.include_router(password_reset_router)
 app.include_router(chat_router)
 app.include_router(voice_query_router)
+app.include_router(favorites_router)
 
