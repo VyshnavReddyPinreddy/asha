dc72bd9c932272c9492de4d2affa6ee6674370f9
VyshnavReddyPinreddy <vyshnavreddy8522@gmail.com>
2026-06-11 22:33:39 +0530

    Add global 401 session expiry handling

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -1,8 +1,31 @@
 import axios from "axios";
+import toast from "react-hot-toast";
+
+let authToastShown = false;
 
 const api = axios.create({
   baseURL: "http://localhost:8000",
   withCredentials: true,
 });
+
+api.interceptors.response.use(
+  (response) => response,
+
+  (error) => {
+    if (error.response?.status === 401 &&
+      !authToastShown) {
+      authToastShown = true;
+      toast.error(
+        "Session expired. Please login again."
+      );
+
+      setTimeout(() => {
+        window.location.href = "/login";
+      }, 2000);
+    }
+
+    return Promise.reject(error);
+  }
+);
 
 export default api;
